3782a720b2692de6e0a94a184ceb57de1de48818
Shani Tripathi <shani.tripathi@axonista.com>
2024-03-19 14:24:28 +0000

    fetch unsplash images on server side

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,30 @@
 import type { LayoutServerLoad } from './$types';
+import type { UnsplashUsersPhotos } from '$lib/types/unsplashResponseTypes';
 import cookie from 'cookie';
+
+const unsplashUri = import.meta.env.VITE_UNSPLASH_URI;
+const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+const username = import.meta.env.VITE_UNSPLASH_USERNAME;
+const url = `${unsplashUri}/users/${username}/photos?client_id=${accessKey}`;
 
 export const load: LayoutServerLoad = async (input) => {
 	const cookies = cookie.parse(input.request.headers.get('cookie') || '');
 	const themeCookie = cookies['theme'] || 'dark';
 
+	let unsplashPhotos: UnsplashUsersPhotos = undefined;
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
+		}
+		unsplashPhotos = await response.json();
+	} catch (e) {
+		console.error('error fetching unsplash images', e);
+	}
+
 	return {
-		theme: themeCookie
+		theme: themeCookie,
+		unsplashPhotos
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import MetaTags from '$components/MetaTags.svelte';
 	import Homescreen from '$lib/screens/Homescreen/Homescreen.svelte';
+	import { unsplashImageStore } from '$stores/unsplashImageStore';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: $unsplashImageStore = data.unsplashPhotos;
 </script>
 
 <MetaTags title="Shani Tripathi" description="Developer, photographer, and a person." />
