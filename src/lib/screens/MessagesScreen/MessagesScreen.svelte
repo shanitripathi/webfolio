30300b3d990301681cdb18f8a93ccecd666ab51b
Shani <personal@shani.local>
2024-08-04 16:01:20 +0100

    format

--- src/lib/screens/MessagesScreen/MessagesScreen.svelte
+++ src/lib/screens/MessagesScreen/MessagesScreen.svelte
@@ -18,13 +18,13 @@
 	};
 </script>
 
-<section class="w-full h-full">
+<section class="h-full w-full">
 	{#if messages.length === 0}
 		<p class="text-sm">
 			Oops! No messages here. It's emptier than Kakashi's face under the mask! 🌀
 		</p>
 	{:else}
-		<h1 class="flex items-center gap-2 mb-8 text-2xl font-medium tracking-tighter">all messages</h1>
+		<h1 class="mb-8 flex items-center gap-2 text-2xl font-medium tracking-tighter">all messages</h1>
 		<ul class="w-full space-y-2">
 			{#each messages as message}
 				<li class="w-full">
